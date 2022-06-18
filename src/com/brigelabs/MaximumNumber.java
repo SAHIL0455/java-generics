0d3208a9568ff88cbee772c356f11c1edd6b7e54
SAHIL0455 <sahilpawar1004@gmail.com>
2022-06-18 12:32:14 +0530

    ADDED: Given the 3 Integers Number and Print the Maximum Number

--- src/com/brigelabs/MaximumNumber.java
+++ src/com/brigelabs/MaximumNumber.java
@@ -1,8 +1,32 @@
 package com.brigelabs;
 
+import java.util.Scanner;
+
 public class MaximumNumber {
+    public static Scanner scanner = new Scanner(System.in);
+    public static int first,second,third;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Generic program");
+        getInput();
+        maxIntegers(first,second,third);
+    }
+    private static void getInput() {
+        System.out.println("Enter the First Integer Number");
+        first = scanner.nextInt();
+        System.out.println("Enter the Second Integer Number");
+        second = scanner.nextInt();
+        System.out.println("Enter the Third Integer Number");
+        third = scanner.nextInt();
+    }
+    private static void maxIntegers(Integer a,Integer b,Integer c){
+        int max = a;
+        if (b.compareTo(max)>0){
+            max = b;
+        }
+        if (c.compareTo(max)>0){
+            max = c;
+        }
+        System.out.println("Maximum Number is "+max);
     }
 }
