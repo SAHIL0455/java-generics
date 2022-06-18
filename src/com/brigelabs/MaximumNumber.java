e2385a67336b06cd3ee3fc3fbb04618813364b0d
SAHIL0455 <sahilpawar1004@gmail.com>
2022-06-18 12:49:31 +0530

    ADDED: Given 3 Floats Number and Print the Maximum Number

--- src/com/brigelabs/MaximumNumber.java
+++ src/com/brigelabs/MaximumNumber.java
@@ -5,13 +5,37 @@ import java.util.Scanner;
 public class MaximumNumber {
     public static Scanner scanner = new Scanner(System.in);
     public static int first,second,third;
+    public static float first1,second1,third1;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Generic program");
-        getInput();
+        getInputForInteger();
         maxIntegers(first,second,third);
+        getInputForFloat();
+        maxFloat(first1,second1,third1);
     }
-    private static void getInput() {
+
+    private static void maxFloat(Float first1, Float second1, Float third1) {
+        float max = first1;
+        if (second1.compareTo(max)>0){
+            max = second1;
+        }
+        if (third1.compareTo(max)>0){
+            max = third1;
+        }
+        System.out.println("Maximum Number is Float "+max);
+    }
+
+    private static void getInputForFloat() {
+        System.out.println("Enter the First Float Number");
+        first1 = scanner.nextFloat();
+        System.out.println("Enter the Second Float Number");
+        second1 = scanner.nextFloat();
+        System.out.println("Enter the Third Float Number");
+        third1 = scanner.nextFloat();
+    }
+
+    private static void getInputForInteger() {
         System.out.println("Enter the First Integer Number");
         first = scanner.nextInt();
         System.out.println("Enter the Second Integer Number");
@@ -27,6 +51,6 @@ public class MaximumNumber {
         if (c.compareTo(max)>0){
             max = c;
         }
-        System.out.println("Maximum Number is "+max);
+        System.out.println("Maximum Number is Integer "+max);
     }
 }
