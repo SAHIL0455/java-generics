6d5741bea2410a277c1621aee6a990c6242b3cd2
SAHIL0455 <sahilpawar1004@gmail.com>
2022-06-19 11:40:11 +0530

    ADDED: Extend the maxParameter method to take more then 3 parameter

--- src/com/brigelabs/MaximumNumber.java
+++ src/com/brigelabs/MaximumNumber.java
@@ -1,35 +1,29 @@
 package com.brigelabs;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MaximumNumber {
     public static Scanner scanner = new Scanner(System.in);
-    public static int first, second, third;
-    public static float first1, second1, third1;
-    public static String first2, second2, third2;
+    public static int first, second, third, fourth;
+    public static float first1, second1, third1, fourth1;
+    public static String first2, second2, third2, fourth2;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Generic program");
         getInputForInteger();
-        int maxInt = maximum(first, second, third);
+        int maxInt = maxParameter(first, second, third,fourth);
         System.out.println("maximum number is "+maxInt);
         getInputForFloat();
-        float maxfloat = maximum(first1, second1, third1);
+        float maxfloat = maxParameter(first1, second1, third1,fourth1);
         System.out.println("Maximum number of float is "+maxfloat);
         getInputForString();
-        String maxString = maximum(first2, second2, third2);
+        String maxString = maxParameter(first2, second2, third2,fourth2);
         System.out.println("Maximum string is "+maxString);
     }
-
-    public static <E extends Comparable> E maximum(E a, E b, E c) {
-        E max = a;
-        if (b.compareTo(max) > 0) {
-            max = b;
-        }
-        if (c.compareTo(max) > 0) {
-            max = c;
-        }
-        return max;
+    public static <T> T maxParameter(T ... a){
+        Arrays.sort(a);
+        return a[a.length-1];
     }
 
     private static void getInputForString() {
@@ -39,6 +33,8 @@ public class MaximumNumber {
         second2 = scanner.next();
         System.out.println("Enter the Third String");
         third2 = scanner.next();
+        System.out.println("Enter the Forth String");
+        fourth2 = scanner.next();
     }
 
     private static void getInputForFloat() {
@@ -48,6 +44,8 @@ public class MaximumNumber {
         second1 = scanner.nextFloat();
         System.out.println("Enter the Third Float Number");
         third1 = scanner.nextFloat();
+        System.out.println("Enter the Forth Float Number");
+        fourth1 = scanner.nextFloat();
     }
 
     private static void getInputForInteger() {
@@ -57,5 +55,7 @@ public class MaximumNumber {
         second = scanner.nextInt();
         System.out.println("Enter the Third Integer Number");
         third = scanner.nextInt();
+        System.out.println("Enter the Forth Integer Number");
+        fourth = scanner.nextInt();
     }
 }
